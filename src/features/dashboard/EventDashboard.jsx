980cad74687d2395dd50d1d3f1f87d575befac38
AlexCsanyi <alex.csanyi@gmail.com>
2020-10-10 15:19:01 +0100

    TRC-9 - set up Redux for dashboard

--- src/features/dashboard/EventDashboard.jsx
+++ src/features/dashboard/EventDashboard.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
-import { data } from "../../app/api/testData";
+import { useSelector } from "react-redux";
 
 export default function EventDashboard() {
-  const [events, setEvents] = useState(data);
+  const { events } = useSelector((state) => state.event);
 
   // function handleCreateEvent(event) {
   //   setEvents([...events, event]);
@@ -17,7 +17,7 @@ export default function EventDashboard() {
   // }
 
   function handleDeleteEvent(eventId) {
-    setEvents(events.filter((event) => event.id !== eventId));
+    // setEvents(events.filter((event) => event.id !== eventId));
   }
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "../src/app/layout/styles.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
+import { configureStore } from "./app/store/configureStore";
 
 const rootElement = document.getElementById("root");
+const store = configureStore();
 
 function render() {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootElement
   );
 }
