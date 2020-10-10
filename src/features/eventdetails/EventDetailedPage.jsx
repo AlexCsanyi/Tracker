d470b11103576dd4c64a5f9001b648c944372124
AlexCsanyi <alex.csanyi@gmail.com>
2020-10-10 13:03:27 +0100

    TRC-7 - worked on post details page

--- src/features/eventdetails/EventDetailedPage.jsx
+++ src/features/eventdetails/EventDetailedPage.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import EventDetailChat from "./EventDetailChat";
+import EventDetailHeader from "./EventDetailHeader";
+import EventDetailInfo from "./EventDetailInfo";
+import EventDetailSidebar from "./EventDetailSidebar";
 
 export default function EventDetailedPage() {
   return (
-    <div>
-      <h1>Event Details</h1>
-    </div>
+    <Grid>
+      <Grid.Column width={10}>
+        <EventDetailHeader />
+        <EventDetailInfo />
+        <EventDetailChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <EventDetailSidebar />
+      </Grid.Column>
+    </Grid>
   );
 }
