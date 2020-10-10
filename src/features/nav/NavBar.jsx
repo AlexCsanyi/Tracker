c87b399a21b58bb7e0b4b6df00f7086271015fa9
AlexCsanyi <alex.csanyi@gmail.com>
2020-10-10 12:08:59 +0100

    TRC-2 - restrcuture folders, set up initial layouts, add test data

---
+++ src/features/nav/NavBar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { Button, Container, Menu } from "semantic-ui-react";
+import SignedInMenu from "./SignedInMenu";
+import SignedOutMenu from "./SignedOutMenu";
+
+export default function NavBar({ setFormOpen }) {
+  const history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
+
+  function handleSignOut() {
+    setAuthenticated(false);
+    history.push("/");
+  }
+
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item as={NavLink} exact to="/" header>
+          <img src="/assets/logo.png" alt="logo" style={{ marginRight: 15 }} />
+          Tracker
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/events" name="Events"></Menu.Item>
+        {authenticated && (
+          <Menu.Item as={NavLink} to="/createEvent">
+            <Button
+              onClick={() => setFormOpen(true)}
+              inverted
+              positive
+              content="Create Event"
+            ></Button>
+          </Menu.Item>
+        )}
+        {authenticated ? (
+          <SignedInMenu signOut={handleSignOut}></SignedInMenu>
+        ) : (
+          <SignedOutMenu setAuthenticated={setAuthenticated}></SignedOutMenu>
+        )}
+      </Container>
+    </Menu>
+  );
+}
